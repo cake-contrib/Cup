dbde249d79b00de714755072bed9ed4db3a2eb85
Patrik Svensson <patrik@patriksvensson.se>
2018-03-02 11:47:44 +0100

    Added help information.

--- src/Cup/Commands/GithubSettings.cs
+++ src/Cup/Commands/GithubSettings.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using Spectre.CommandLine;
 
 namespace Cup.Commands
@@ -5,9 +6,11 @@ namespace Cup.Commands
     public class GithubSettings
     {
         [CommandArgument(0, "<USER>")]
+        [Description("The GitHub username to use.")]
         public string User { get; set; }
 
         [CommandArgument(1, "<TOKEN>")]
+        [Description("The GitHub access token to use for the provided user.")]
         public string Token { get; set; }
     }
 }
--- src/Cup/Commands/Update/UpdateSettings.cs
+++ src/Cup/Commands/Update/UpdateSettings.cs
@@ -9,22 +9,28 @@ namespace Cup.Commands.Update
     public class UpdateSettings : GithubSettings
     {
         [CommandArgument(0, "<REPOSITORY>")]
+        [Description("The repository to update (owner/repository).")]
         public string Repository { get; [UsedImplicitly] set; }
 
-        [CommandArgument(0, "<VERSION>")]
+        [CommandArgument(1, "<VERSION>")]
+        [Description("The Cake version to update to.")]
         public string Version { get; [UsedImplicitly] set; }
 
-        [CommandOption("-w|--working <PATH>")]
+        [CommandOption("--working <PATH>")]
         [TypeConverter(typeof(DirectoryPathConverter))]
+        [Description("The working directory to use.")]
         public DirectoryPath WorkingDirectory { get; [UsedImplicitly] set; }
 
-        [CommandOption("-c|--commit")]
+        [CommandOption("--commit")]
+        [Description("Determines wheter or not to create a commit.")]
         public bool Commit { get; [UsedImplicitly] set; }
 
-        [CommandOption("-p|--push")]
+        [CommandOption("--push")]
+        [Description("Determines wheter or not to push the commit. Requires the --commit parameter.")]
         public bool Push { get; [UsedImplicitly] set; }
 
         [CommandOption("--pr")]
+        [Description("Determines wheter or not to create a PR. Requires the --push parameter.")]
         public bool OpenPullRequest { get; [UsedImplicitly] set; }
     }
 }
